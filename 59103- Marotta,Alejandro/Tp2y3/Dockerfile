625506dec2fb43eb938c4d8a1daebf45abea8845
amarotta1 <alitomarotta@gmail.com>
2020-05-20 14:26:46 -0300

    Agregando TP3

---
+++ 59103- Marotta,Alejandro/Tp2y3/Dockerfile
@@ -0,0 +1,18 @@
+  
+FROM python:3
+
+
+
+RUN git clone https://github.com/amarotta1/CajeroAutomatico.git
+
+
+
+WORKDIR /CajeroAutomatico
+
+
+
+RUN pip install -r requirements.txt
+
+
+
+CMD ["python3", "./test_cajero_ex.py"]
---
+++ 59103- Marotta,Alejandro/Tp2y3/excepciones.py
@@ -0,0 +1,17 @@
+class MontoError(Exception):
+    pass
+
+class MontoMayor(Exception):
+    pass
+
+class CombinacionError(Exception):
+    pass
+
+class PorcentajeError(Exception):
+    pass
+
+class MontoNegativo(Exception):
+    pass
+
+class CajeroVacio(Exception):
+    pass
